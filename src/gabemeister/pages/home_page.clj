40a1066704463a0363e9033d9636d9593e56f9ad
Gabe Jensen <thegabejensen@gmail.com>
2024-05-23 07:56:15 -0700

    Modified homepage

--- src/gabemeister/pages/home_page.clj
+++ src/gabemeister/pages/home_page.clj
@@ -6,10 +6,16 @@
 
 (defn home-page
   []
-  (let [projects (get-all-repos)]
+  (let []
     (templ
      (page-wrapper [:div
-                    {:class "p-6"}
-                    [:ul
-                     (for [proj projects]
-                       (repo-list-item proj))]]))))
+                    {:class "p-12 bg-cyan-950 min-h-screen"}
+                    [:div
+                     {:class "flex flex-col justify-center items-center text-white"}
+                     [:h1
+                      {:class "text-5xl text-yellow-400"}
+                      "Year End Recap"]
+                     [:span {:class "mt-6 text-gray-300 italic"} "To install, run the following in your terminal:"]
+                     [:span
+                      {:class "inline-block mt-6 text-xl"}
+                      "curl -fsSL https://yer-app.fly.dev/install | bash"]]]))))
--- src/gabemeister/templates/page_wrapper.clj
+++ src/gabemeister/templates/page_wrapper.clj
@@ -15,9 +15,7 @@
 
     [:link {:rel "preconnect" :href "https://fonts.googleapis.com"}]
     [:link {:rel "preconnect" :href "https://fonts.gstatic.com"}]
-    [:link {:href "https://fonts.googleapis.com/css2?family=Lato&display=swap"
-            :rel "stylesheet"}]
-    [:link {:href "https://fonts.googleapis.com/css2?family=Bitter&family=Lato&display=swap"
+    [:link {:href "https://fonts.googleapis.com/css2?family=Bitter&family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&family=Lato&display=swap"
             :rel "stylesheet"}]]
    [:body
     content]])
